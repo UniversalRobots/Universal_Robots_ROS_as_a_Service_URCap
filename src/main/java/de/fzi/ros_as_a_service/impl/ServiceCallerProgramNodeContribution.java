b85876296e80f5cb5d4e3f6bb16f5be8630e270e
Felix Exner <exner@fzi.de>
2021-04-13 13:46:52 +0200

    Updated service implementation to current structure
    View part works, only the code generation has to be implemented, I think.

--- src/main/java/de/fzi/ros_as_a_service/impl/ServiceCallerProgramNodeContribution.java
+++ src/main/java/de/fzi/ros_as_a_service/impl/ServiceCallerProgramNodeContribution.java
@@ -25,103 +25,67 @@
 
 package de.fzi.ros_as_a_service.impl;
 
-import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
 import com.ur.urcap.api.domain.data.DataModel;
 import com.ur.urcap.api.domain.script.ScriptWriter;
-import com.ur.urcap.api.domain.undoredo.UndoRedoManager;
-import com.ur.urcap.api.domain.undoredo.UndoableChanges;
 
-public class ServiceCallerProgramNodeContribution implements ProgramNodeContribution {
-  private final ProgramAPIProvider apiProvider;
+public class ServiceCallerProgramNodeContribution extends RosTaskProgramSuperNodeContribution {
   private final ServiceCallerProgramNodeView view;
-  private final DataModel model;
-  private final UndoRedoManager undoRedoManager;
-
-  private static final String MASTER_KEY = "master";
-  private static final String DEFAULT_MASTER = "";
-  private static final String TOPIC_KEY = "topic";
-  private static final String DEFAULT_TOPIC = "";
 
   public ServiceCallerProgramNodeContribution(
       ProgramAPIProvider apiProvider, ServiceCallerProgramNodeView view, DataModel model) {
-    this.apiProvider = apiProvider;
+    super(apiProvider, model);
     this.view = view;
-    this.model = model;
-    this.undoRedoManager = this.apiProvider.getProgramAPI().getUndoRedoManager();
-  }
-
-  public void onMasterSelection(final String master) {
-    undoRedoManager.recordChanges(new UndoableChanges() {
-      @Override
-      public void executeChanges() {
-        model.set(MASTER_KEY, master);
-      }
-    });
-  }
-
-  public void onTopicSelection(final String master) {
-    undoRedoManager.recordChanges(new UndoableChanges() {
-      @Override
-      public void executeChanges() {
-        model.set(TOPIC_KEY, master);
-      }
-    });
-  }
-
-  private String getMaster() {
-    return model.get(MASTER_KEY, DEFAULT_MASTER);
-  }
-
-  private String getTopic() {
-    return model.get(TOPIC_KEY, DEFAULT_TOPIC);
-  }
-
-  private String[] getMasterList() {
-    // TODO: Get this from installation
-    String[] items = new String[1];
-    items[0] = "192.168.56.1";
-    return items;
-  }
-
-  private String[] getTopicsList() {
-    // TODO: Get this dynamically
-    String[] items = new String[2];
-    items[0] = "";
-    items[1] = "/test_service";
-    return items;
   }
 
   @Override
   public void openView() {
-    view.setMasterComboBoxItems(getMasterList());
-    view.setTopicComboBoxItems(getTopicsList());
-    // view.setMasterComboBoxSelection(getMaster());
+    // TODO: If we parametrize RosTaskSuperNodeContribution with the view type, we get a cyclic
+    // parametrization. So, for now we keep solving this here, but there might be a better solution.
+    super.openView();
+    view.updateView(this);
   }
-
-  @Override
-  public void closeView() {}
 
   @Override
   public String getTitle() {
-    // TODO: Put service name here
-    return "Call " + getTopic();
+    return "Call " + getMsg();
   }
 
   @Override
-  public boolean isDefined() {
-    return getTopic() != "";
+  protected String[] getMsgLayoutKeys() {
+    return new String[] {"Request", "Response"};
   }
 
   @Override
-  public void generateScript(ScriptWriter writer) {
-    String test_string =
-        "{\"op\":\"publish\", \"topic\": \"/test\", \"msg\": {\"data\": \"world\"}}";
-    byte[] bytes = test_string.getBytes();
-    for (int j = 0; j < bytes.length; j++) {
-      writer.appendLine("socket_send_byte(" + bytes[j] + ", "
-          + "\"testserver\""
-          + ")");
-    }
+  protected String[] getMsgLayoutDirections() {
+    return new String[] {"out", "in"};
   }
+
+  @Override
+  protected String getMsgTypeRequestString(final String topic_name) {
+    return "{\"op\": \"call_service\",\"service\":\"/rosapi/service_type\",\"args\":{\"service\":\""
+        + topic_name + "\"}}";
+  }
+
+  @Override
+  protected String getMsgListRequestString() {
+    return "{\"op\": \"call_service\",\"service\": \"/rosapi/services\"}";
+  }
+
+  @Override
+  protected String getMsgListResponsePlaceholder() {
+    return "services";
+  }
+
+  @Override
+  protected String[] getMsgLayoutRequestStrings(final String msg_type) {
+    return new String[] {
+        "{\"op\": \"call_service\",\"service\":\"/rosapi/service_request_details\", \"args\":{\"type\":\""
+            + msg_type + "\"}}",
+        "{\"op\": \"call_service\",\"service\":\"/rosapi/service_response_details\", \"args\":{\"type\":\""
+            + msg_type + "\"}}"};
+  }
+
+  @Override
+  public void generateScript(ScriptWriter writer) {}
 }
