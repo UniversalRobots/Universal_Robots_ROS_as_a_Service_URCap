b24b1daf0b7495126e0788935c634e6234fd99c5
Felix Exner <exner@fzi.de>
2021-04-14 12:28:17 +0200

    Reimplement subscriber parsing capability
    This got lost during the refactoring

--- src/main/java/de/fzi/ros_as_a_service/impl/TopicSubscriberProgramNodeContribution.java
+++ src/main/java/de/fzi/ros_as_a_service/impl/TopicSubscriberProgramNodeContribution.java
@@ -28,8 +28,11 @@ package de.fzi.ros_as_a_service.impl;
 import com.ur.urcap.api.contribution.program.ProgramAPIProvider;
 import com.ur.urcap.api.domain.data.DataModel;
 import com.ur.urcap.api.domain.script.ScriptWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.regex.*;
+import org.json.JSONObject;
 
 public class TopicSubscriberProgramNodeContribution extends RosTaskProgramSuperNodeContribution {
   private final TopicSubscriberProgramNodeView view;
@@ -87,14 +90,11 @@ public class TopicSubscriberProgramNodeContribution extends RosTaskProgramSuperN
   }
 
   private void generateElementParser(String element_name, String source_var, String target_var,
-      boolean targetIsGlobal, boolean numericTarget, ScriptWriter writer) {
+      boolean numericTarget, ScriptWriter writer) {
     writer.assign("bounds", "json_getElement(" + source_var + ", \"" + element_name + "\")");
     String l_val = new String();
     String r_val = new String();
 
-    if (!targetIsGlobal) {
-      l_val = "local ";
-    }
     l_val += target_var;
 
     if (numericTarget) {
@@ -105,24 +105,75 @@ public class TopicSubscriberProgramNodeContribution extends RosTaskProgramSuperN
       r_val += ")";
     }
     writer.assign(l_val, r_val);
+    // writer.appendLine("textmsg(\"" + l_val + "\", " + l_val + ")");
     writer.assign(
         source_var, "ri_reduceString(" + source_var + ", bounds[0], bounds[3]-bounds[0]+1)");
   }
 
-  private void generateValueNodeInstallation(LoadValueNode node, ScriptWriter writer) {
-    if (node.getParent() == null) {
-      return; // this can occur for basenode -> nothing todo here
+  private boolean isMapping(final JSONObject obj) {
+    if (obj.names().length() != 1) {
+      return false;
     }
-    String source_variable = node.getParent().getExtractionFunctionName();
-    String target_variable;
-    if (node.getVariableUsed()) {
-      target_variable = node.getValue();
-    } else {
-      target_variable = node.getExtractionFunctionName();
+
+    String[] field_names = JSONObject.getNames(obj);
+
+    Pattern p = Pattern.compile("-\\+useVar(?<num>Num)?\\+-");
+    Matcher m = p.matcher(field_names[0]);
+    return m.matches();
+  }
+
+  private List<ValueInputNode> getNodesWithVariables(final JSONObject root, ScriptWriter writer) {
+    return getNodesWithVariables(root, "l_msg", writer);
+  }
+
+  private List<ValueInputNode> getNodesWithVariables(
+      final JSONObject root, String name, ScriptWriter writer) {
+    System.out.println("# getNodesWithVariables called with\n" + root.toString(2));
+    List<ValueInputNode> out_list = new ArrayList<ValueInputNode>();
+    List<ValueInputNode> children = new ArrayList<ValueInputNode>();
+
+    System.out.println("Fields to check: " + root.names());
+    Pattern p = Pattern.compile("-\\+useVar(?<num>Num)?\\+-");
+    for (int i = 0; i < root.names().length(); i++) {
+      System.out.println("Checking entry " + root.names().getString(i));
+
+      Object child = root.get(root.names().getString(i));
+      if (child instanceof JSONObject) {
+        JSONObject obj = (JSONObject) child;
+
+        if (isMapping(obj)) {
+          // Get the key and match it
+          System.out.println("Found variable " + obj.getString(obj.names().getString(0)));
+          System.out.println("Key: " + obj.names().getString(0));
+          Matcher m = p.matcher(obj.names().getString(0));
+          if (m.matches()) {
+            String value = obj.getString(obj.names().getString(0));
+            String type = "string";
+            if (m.group("num") != null) {
+              // At this stage we don't know what kind of number it is but for further processing
+              // this might not be of any concern.
+              type = "float64";
+            }
+            String path = name + "/" + root.names().getString(i);
+            ValueInputNode new_node = new ValueInputNode(path, type, value);
+            System.out.println("Adding new node " + new_node);
+            children.add(new_node);
+          } else {
+            System.out.println("Did not match");
+          }
+        } else {
+          children.addAll(getNodesWithVariables(
+              (JSONObject) child, name + "/" + root.names().getString(i), writer));
+        }
+      }
     }
-    String json_key = node.getName();
-    generateElementParser(json_key, source_variable, target_variable, node.getVariableUsed(),
-        node.isNumericType(), writer);
+    if (!children.isEmpty()) {
+      ValueInputNode new_node = new ValueInputNode(name, "string", name.replaceAll("/", "_"));
+      System.out.println("Adding new node " + new_node);
+      out_list.add(new_node);
+      out_list.addAll(children);
+    }
+    return out_list;
   }
 
   @Override
@@ -135,34 +186,26 @@ public class TopicSubscriberProgramNodeContribution extends RosTaskProgramSuperN
 
     System.out.println("Subscriber generate subscriber Script");
     // add parser for values!
-    LoadValueNode base_node = new LoadValueNode(null, null);
-    //    loadValuesToTree(null, new JSONObject(buildJsonString(tree, false)), "l_msg");
-    base_node.setExtractionAdded();
-    List<LoadValueNode> nodes_list = base_node.getVariableUsingNodes(null);
-    ListIterator<LoadValueNode> iterator = nodes_list.listIterator();
+    //
+
+    JSONObject values = getMsgValue(getMsgLayoutKeys()[0]);
+    // System.out.println("Subscription values:\n" + values.toString(2));
 
     writer.defineFunction("parseSubscript" + ID); // add function definition
-    if (!nodes_list.isEmpty()) { // no variables needed -> nothing todo here
-      writer.assign("local l_msg", globalvar);
-      writer.assign("local bounds", "[0, 0, 0, 0]");
+    writer.assign("local l_msg", globalvar);
+    writer.assign("local bounds", "[0, 0, 0, 0]");
 
-      while (iterator.hasNext()) {
-        List<LoadValueNode> path =
-            iterator.next()
-                .getParentPath(); // get path of parents (at least one element contained!)
-        System.out.println(
-            "Generate Script for " + path.get(path.size() - 1).getExtractionFunctionName());
-        ListIterator<LoadValueNode> parent = path.listIterator();
-        while (parent.hasNext()) { // for each node in path
-          LoadValueNode element = parent.next();
-          System.out.println("Generate Extraction for " + element.getExtractionFunctionName());
-          if (!element.isExtractionAdded()) {
-            generateValueNodeInstallation(element, writer);
-            element.setExtractionAdded();
-          } else {
-            System.out.println("... already exists!");
-          }
-        }
+    List<ValueInputNode> nodes_with_variables = getNodesWithVariables(values, writer);
+    System.out.println("Found tree: " + nodes_with_variables);
+    String l_msg = "l_msg";
+    for (int i = 0; i < nodes_with_variables.size(); i++) {
+      String label = nodes_with_variables.get(i).getLabel();
+      String[] elements = label.split("/");
+      String name = elements[elements.length - 1];
+      l_msg = String.join("_", Arrays.copyOfRange(elements, 0, elements.length - 1));
+      if (i > 0) {
+        generateElementParser(name, l_msg, nodes_with_variables.get(i).getValue(),
+            nodes_with_variables.get(i).isNumericType(), writer);
       }
     }
     writer.end(); // end function definition to parse subscript
